939077f606a4ba8e1bde09a39d4a7820d08b0e8b
ruvelbarua <ruvel.br78@gmail.com>
2021-10-31 19:33:43 +0600

    update

--- src/App.js
+++ src/App.js
@@ -17,56 +17,59 @@ import ServiceDetails from './components/ServiceDetails/ServiceDetails';
 import initializeFirebase from './components/Firebase/firebase.init';
 import AddUsers from './components/AddUsers/AddUsers';
 import Users from './components/Users/Users';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import AuthProvider from './components/Context/AuthProvider';
 initializeFirebase();
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Header></Header>
-        <Switch>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
-          <Route exact path="/home">
-            <Home></Home>
-          </Route>
-          <Route exact path="/services">
-            <Services></Services>
-          </Route>
-          <Route exact path="/service">
-            <Service></Service>
-          </Route>
-          <Route path="/sdetails/:id">
-            <ServiceDetails></ServiceDetails>
-          </Route>
-          <Route exact path="/morder">
-            <ManageOrder></ManageOrder>
-          </Route>
-          <Route exact path="/myorder">
-            <MyOrder></MyOrder>
-          </Route>
-          <Route exact path="/about">
-            <About></About>
-          </Route>
-          <Route exact path="/adduser">
-            <AddUsers></AddUsers>
-          </Route>
-          <Route exact path="/users">
-            <Users></Users>
-          </Route>
-
-          <Route exact path="/register">
-            <Registration></Registration>
-          </Route>
-          <Route exact path="/login">
-            <Login></Login>
-          </Route>
-          <Route exact path="*">
-            <NotFound></NotFound>
-          </Route>
-        </Switch>
-        <Footer></Footer>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Header></Header>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route exact path="/home">
+              <Home></Home>
+            </Route>
+            <Route exact path="/services">
+              <Services></Services>
+            </Route>
+            <Route exact path="/service">
+              <Service></Service>
+            </Route>
+            <PrivateRoute path="/sdetails/:id">
+              <ServiceDetails></ServiceDetails>
+            </PrivateRoute>
+            <Route exact path="/morder">
+              <ManageOrder></ManageOrder>
+            </Route>
+            <Route exact path="/myorder">
+              <MyOrder></MyOrder>
+            </Route>
+            <Route exact path="/about">
+              <About></About>
+            </Route>
+            <Route exact path="/adduser">
+              <AddUsers></AddUsers>
+            </Route>
+            <Route exact path="/users">
+              <Users></Users>
+            </Route>
+            <Route exact path="/register">
+              <Registration></Registration>
+            </Route>
+            <Route exact path="/login">
+              <Login></Login>
+            </Route>
+            <Route exact path="*">
+              <NotFound></NotFound>
+            </Route>
+          </Switch>
+          <Footer></Footer>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
